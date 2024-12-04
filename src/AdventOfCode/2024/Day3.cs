8f4c9fc20ddd4c8c0d1f2d8b6ee041e561af90c9
Benjamin Nomine <115967+benomine@users.noreply.github.com>
2024-12-03 21:31:48 -0500

    feat: day 3

---
+++ src/AdventOfCode/2024/Day3.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode._2024
+{
+    public partial class Day3 : IDay
+    {
+        public DateTime Date => new DateTime(2024, 12, 3);
+        public bool IsIgnored => false;
+
+        [GeneratedRegex(@"mul\((?<left>\d{1,3})\,(?<right>\d{1,3})\)")]
+        private static partial Regex MulRegex();
+
+        [GeneratedRegex(@"do\(\)")]
+        private static partial Regex DoRegex();
+
+        [GeneratedRegex(@"don't\(\)")]
+        private static partial Regex DontRegex();
+
+        public (string result, TimeSpan timeTaken) SolvePart1(string input)
+        {
+            var start = Stopwatch.GetTimestamp();
+            var total = 0;
+
+            var matches = MulRegex().Matches(input);
+            foreach (Match match in matches)
+            {
+                total += int.Parse(match.Groups["left"].ValueSpan) * int.Parse(match.Groups["right"].ValueSpan);
+            }
+
+            return (total.ToString(), Stopwatch.GetElapsedTime(start));
+        }
+
+        public (string result, TimeSpan timeTaken) SolvePart2(string input)
+        {
+            var start = Stopwatch.GetTimestamp();
+
+            var total = 0;
+
+            var matches = MulRegex().Matches(input);
+            var doMatches = DoRegex().Matches(input).Select(x => x.Index).ToList();
+            var dontMatches = DontRegex().Matches(input).Select(x => x.Index).ToList();
+
+            var merged = doMatches.Concat(dontMatches).OrderBy(x => x).ToArray();
+            var next = 0;
+
+            var isEnabled = true;
+
+            foreach (Match match in matches)
+            {
+                var matchedIndex = match.Index;
+                if (next < merged.Length && matchedIndex > merged[next])
+                {
+                    var isDo = doMatches.Contains(merged[next]);
+                    isEnabled = isDo;
+                    next++;
+                }
+
+                if (isEnabled)
+                {
+                    total += int.Parse(match.Groups["left"].ValueSpan) *
+                             int.Parse(match.Groups["right"].ValueSpan);
+                }
+            }
+
+            return (total.ToString(), Stopwatch.GetElapsedTime(start));
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/2024/Day3Tests.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode._2024;
+
+namespace AdventOfCode.Tests._2024
+{
+    public class Day3Tests : IDayTest
+    {
+        private const string _input1 = 
+            """
+            xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+            """;
+
+        private const string _input2 =
+            """
+            xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+            """;
+        
+        private const string _result1 = "161";
+        private const string _result2 = "48";
+
+        private readonly IDay _sut = new Day3();
+        
+        [Fact]
+        public void SolvePart1()
+        {
+            var (result, _) = _sut.SolvePart1(_input1);
+            
+            result.Should().Be(_result1);
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            var (result, _) = _sut.SolvePart2(_input2);
+            
+            result.Should().Be(_result2);
+        }
+    }
+}
